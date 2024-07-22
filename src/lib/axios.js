bc7312714b94d29982cc18125caba356a4fe4957
farhan0212 <farhan.ramadan0212@gmail.com>
2024-07-22 20:30:54 +0700

    method get

---
+++ src/lib/axios.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const axiosIstance = axios.create({
+  baseURL: "http://localhost:2000",
+});
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,47 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import {
+  Container,
+  Heading,
+  Table,
+  Td,
+  Th,
+  Tr,
+  Thead,
+  Tbody,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { axiosIstance } from "@/lib/axios";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async (req, res) => {
+    try {
+      const productResponse = await axiosIstance.get("/products");
+      setProducts(productResponse.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderProducts = () => {
+    return products.map((product) => {
+      return (
+        <Tr key={product.id}>
+          <Td>{product.id}</Td>
+          <Td>{product.name}</Td>
+          <Td>{product.price}</Td>
+          <Td>{product.description}</Td>
+          <Td>{product.image}</Td>
+        </Tr>
+      );
+    });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,100 +50,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <Container>
+          <Heading>Hello World</Heading>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Price</Th>
+                <Th>Description</Th>
+                <Th>image</Th>
+              </Tr>
+            </Thead>
+            <Tbody>{renderProducts()}</Tbody>
+          </Table>
+        </Container>
       </main>
     </>
   );
